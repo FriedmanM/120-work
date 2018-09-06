7fd2d185bb015bde047ec87b955b069cb08a608c
FriedmanM <31631790+FriedmanM@users.noreply.github.com>
2018-09-06 13:15:24 -0600

    created hw-3, 01_hello-world
    Created hw-3 structure and ran first assignment. Commit for 01_hello-world test project.

---
+++ hw-3/sketch.js
@@ -0,0 +1,18 @@
+// Declares the setup function
+function setup() {
+
+  //Creates a canvas of size specified in parentheses
+  createCanvas( 600, 400 );
+
+  //Sets background color to color specified
+  background( 'green' );
+}
+//Declares a function named 'draw'
+function draw() {
+
+  /* Creates the text 'Hello World!' on the page.
+     This text gets aligned to the x and y axis
+     at x: 20, y: 30 */
+  text( "Hello World!", 20, 30 );
+
+}
