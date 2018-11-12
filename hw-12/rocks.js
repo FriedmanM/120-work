e7fa4797f657e024d0df639f9faf6ced8534d031
FriedmanM <31631790+FriedmanM@users.noreply.github.com>
2018-11-11 22:59:01 -0700

    Added rocks
    Added rock class. Creating rocks randomly and making them die after wall touch.

---
+++ hw-12/rocks.js
@@ -0,0 +1,38 @@
+class Rocks {
+  constructor(x, y, r) {
+    this.color = 'brown';
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.deltaX = random(-10, 10);
+    this.deltaY = random(-10, 10);
+  }
+
+  display() {
+    push();
+    fill(this.color);
+    translate(this.x, this.y);
+    ellipse(0, 0, this.r);
+    pop();
+  }
+
+  move() {
+    this.x += this.deltaX;
+    this.y += this.deltaY;
+  }
+
+  edgeCheck(r) {
+    if (this.x + this.r >= width || this.x - this.r <= 0) {
+      for (let i = r.length - 1; i >= 0; i--) {
+          r.splice(i, 1);
+        }
+      }
+    }
+// check if the ball has hit a horizontal wall (top or bottom walls)
+    if (this.posY + this.rad >= height || this.posY - this.rad <= 0) {
+      for (let i = r.length - 1; i >= 0; i--) {
+          r.splice(i, 1);
+      }
+    }
+  }
+}
--- hw-12/sketch.js
+++ hw-12/sketch.js
@@ -1,35 +1,37 @@
 var gameOver = false;
 var link = [];
+var rocks = [];
 
 function setup() {
 	createCanvas(800, 600);
   let c = new Character(width/2, height/2, 25);
+  let b = new Rock(width/2, height/2, 10);
   link.push(c);
+  rocks.push(b);
+  rockTimer();
 }
 
 function draw() {
   background( 'rgb(144, 205, 96)' );
-  
-  link[0].display();
-  link[0].move();
+  if (!gameover) {
+    link[0].display();
+    link[0].move();
 
-/*	var playerDistance = dist(playerX, playerY);
-		//if(playerDistance < 50) {
-			// THEYVE COLLIDED! END GAME!
-			//gameOver = true;
-		}
-
-	 else {
-
-		// if game IS over...
+    for (let i = 0; i < bubbles.length; i++) {
+        rocks[i].move();
+        rocks[i].show();
+        rocks[i].edgeCheck(rocks);
+    }
+  }
+  else {
+		// If game is over, end game and display text
 		fill(255,44,0);
 		textSize(60);
 		text("Game Over", width/2, height/2);
-*/
 
-  //title of game
+  //Game Title
   push();
-  fill( "white" );
+  fill( 'rgb(24, 77, 13)' );
   textAlign(CENTER);
   textSize(35);
   text("Link to P5st", width/2, 35);
@@ -37,7 +39,14 @@ function draw() {
 
   push();
   strokeWeight(4);
-  stroke( "white" );
+  stroke( 'rgb(24, 77, 13)' );
   line( 0, 45, width, 45 );
   pop();
+}
+
+function rockTimer() {
+    let b = new Rock(random(width), random(height), 10;
+    rock.push(b);
+    // this has the effect of calling itself every x-milliseconds
+    setTimeout(rockTimer, random(2000));
 }
