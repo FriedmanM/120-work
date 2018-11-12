4fbc743e445f9b2866edb028e5625af5626f5aef
FriedmanM <31631790+FriedmanM@users.noreply.github.com>
2018-11-11 21:00:48 -0700

    Testing scroll bars 2

--- hw-12/sketch.js
+++ hw-12/sketch.js
@@ -1,6 +1,10 @@
 var gameOver = false;
 var link = [];
 
+function unloadScrollBars() {
+    document.documentElement.style.overflow = 'hidden';  // firefox, chrome
+    document.body.scroll = "no"; // ie only
+}
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
