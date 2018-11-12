ee98b777efaaf78179f86f4c414bd42309b7a6ee
FriedmanM <31631790+FriedmanM@users.noreply.github.com>
2018-11-11 23:29:09 -0700

    Testing title text colors

--- hw-12/sketch.js
+++ hw-12/sketch.js
@@ -39,7 +39,7 @@ function draw() {
 
   //Game Title
   push();
-  fill( 'rgb(24, 77, 13)' );
+  fill( 'black' );
   textAlign(CENTER);
   textSize(35);
   text("Link to P5st", width/2, 35);
@@ -47,7 +47,7 @@ function draw() {
 
   push();
   strokeWeight(4);
-  stroke( 'rgb(24, 77, 13)' );
+  stroke( 'black' );
   line( 0, 45, width, 45 );
   pop();
   }
