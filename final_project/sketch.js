02f8100fe6c1f27e8e3422ca1a94a4e9c6fbe57a
FriedmanM <31631790+FriedmanM@users.noreply.github.com>
2018-12-11 23:27:53 -0700

    Built Game Mechanics
    Built all game mechanics. Paddle bumps ball, made more visual pleasing, created "AI" PC.

--- final_project/sketch.js
+++ final_project/sketch.js
@@ -1,15 +1,29 @@
-let xBall = 320;
-let yBall = 180;
-let xSpeed = 5;
-let ySpeed = 2;
+//Ball Variables
+var xBall = 320;
+var yBall = 180;
+var xSpeed = 5;
+var ySpeed = 2;
+var s = 12.5;
 
-let widthPaddle = 20;
+//Paddle Variables
+var widthPaddle = 20;
 
-let leftPaddleX = 10;
+var leftPaddleX = 10;
+var leftPaddleY = 1;
 
-let rightPaddleX = 770;
+var rightPaddleX = 770;
+var rightPaddleY = 1;
+var rightPaddleS = 0.75;
 
-let s = 12.5;
+//Boundary Variables
+var mx = 1;
+var easing = 0.05;
+var edge = 2;
+var inner = edge + s;
+
+//Score Variables
+var playerScore = 0;
+var pcScore = 0;
 
 function setup() {
   createCanvas( 800, 600 );
@@ -18,8 +32,7 @@ function setup() {
 function draw() {
   background( 'rgb(0, 0, 0)' );
 
-  push();
-  noCursor();
+  //noCursor();
 
   push();
   fill( 'rgb(105, 252, 102)' );
@@ -27,21 +40,40 @@ function draw() {
   text("Have fun!", 40, 45 );
   pop();
 
-  //Left Paddle
   push();
-  fill( 'rgb(105, 252, 102)' );
-  rect( leftPaddleX, mouseY, widthPaddle, 150 );
+  stroke( "white" );
+  strokeWeight(10);
+  line(400, 0, 400, 25);
+  line(400, 50, 400, 75);
+  line(400, 100, 400, 125);
+  line(400, 150, 400, 175);
+  line(400, 200, 400, 225);
+  line(400, 250, 400, 275);
+  line(400, 300, 400, 325);
+  line(400, 350, 400, 375);
+  line(400, 400, 400, 425);
+  line(400, 450, 400, 475);
+  line(400, 500, 400, 525);
+  line(400, 550, 400, 575);
+  line(400, 600, 400, 625);
   pop();
 
   push();
-  fill( 'rgb(105, 252, 102)' );
-  rect( rightPaddleX, mouseY, widthPaddle, 150 );
+  stroke( "white" );
+  line(0, 60, 800, 60);
   pop();
+
+  //Left Paddle Function
+  leftPaddle();
+
+  //Right Paddle Function
+  rightPaddle();
+
 
   ball();
   ballMovement();
+  paddleBounce();
 
-  pop();
 }
 
 function ball() {
@@ -60,6 +92,77 @@ function ballMovement() {
   }
 }
 
-function paddleMax() {
+/*
+    I am using the code in the left paddle (constrain) to keep the paddle in bounds.
+    I found the code here: https://p5js.org/examples/input-constrain.html
+    Since I am using code that is not mine, I will explain what it does.
 
+    The code takes the absolute value of the mouseY value, minus the leftPaddleY value,
+    which is a constrain function that calculates the borders of the canvas.
+
+    if the value is greater than 0.1, leftPaddleY is added to mouseY minus leftPaddleY * the easing
+    (a tiny border inside canvas) in order to block movement outside canvas.
+*/
+
+
+
+function leftPaddle() {
+  fill( 'rgb(105, 252, 102)' );
+  rect( leftPaddleX, leftPaddleY, widthPaddle, 150 );
+  if (abs(mouseY - leftPaddleY) > 0.1) {
+    leftPaddleY = leftPaddleY + (mouseY - leftPaddleY) * easing;
+  }
+  leftPaddleY = constrain(leftPaddleY, 0, height-150);
 }
+
+function rightPaddle() {
+  //var paddleSpeed = yBall - .05;
+  //rightPaddleY = constrain(paddleSpeed, 0, height-150);
+  //rightPaddleY = (yBall - 70) * rightPaddleS;
+  fill( 'rgb(105, 252, 102)' );
+  rect( rightPaddleX, rightPaddleY, widthPaddle, 150 );
+  if (abs(yBall - rightPaddleY) > 0.1) {
+    rightPaddleY = rightPaddleY + (yBall - rightPaddleY) * easing;
+  }
+  rightPaddleY = constrain(rightPaddleY, 0, height-150);
+}
+
+function paddleBounce() {
+  //left paddle bounce mechanism
+  if ( (xBall < 30 && xBall > 20) ) {
+    if ((yBall > leftPaddleY & yBall < leftPaddleY+50)) {
+      xSpeed *= -1;
+      if (ySpeed == 0) {
+        ySpeed = -2;
+      }
+    }
+    else if ((yBall > leftPaddleY+50 & yBall < leftPaddleY+100)) {
+      xSpeed *= -1;
+      ySpeed = 0;
+    }
+    if ((yBall > leftPaddleY+100 & yBall < leftPaddleY+150)) {
+      xSpeed *= -1;
+      if (ySpeed == 0) {
+        ySpeed = -2;
+      }
+    }
+  }
+  //Right paddle bounce mechanism
+  if ( (xBall < 755 && xBall > 745) ) {
+    if ((yBall > rightPaddleY & yBall < rightPaddleY+50)) {
+      xSpeed *= -1;
+      if (ySpeed == 0) {
+        ySpeed = -2;
+      }
+    }
+    else if ((yBall > rightPaddleY+50 & yBall < rightPaddleY+100)) {
+      xSpeed *= -1;
+      ySpeed = 0;
+    }
+    if ((yBall > rightPaddleY+100 & yBall < rightPaddleY+150)) {
+      xSpeed *= -1;
+      if (ySpeed == 0) {
+        ySpeed = -2;
+      }
+    }
+  }
